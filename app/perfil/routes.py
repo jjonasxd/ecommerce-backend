3b0499d0aa2f8787ee0107d874417e09de15d947
jjonas <joaosoares3798@gmail.com>
2026-07-23 22:14:07 -0300

    rota /api/refresh feita com sucesso

--- app/perfil/routes.py
+++ app/perfil/routes.py
@@ -1,22 +1,30 @@
 from flask import Flask, Blueprint, request
 from app.perfil.services import *
-from flask_jwt_extended import get_jwt_identity, create_access_token
+from flask_jwt_extended import get_jwt_identity, create_access_token, set_access_cookies
 
 BP_perfil = Blueprint('rotas_perfil', __name__)
 
 @BP_perfil.route('/api/perfil-dados', methods=['GET'])
 @jwt_required()
-def configs():
+def dados_do_perfil():
     user_id = get_jwt_identity()
     print(user_id, flush=True)
-    resposta = resgatar_dados_usuario(user_id)
 
-    return resposta
+    return resgatar_dados_usuario(user_id)
 
 @BP_perfil.route('/api/refresh', methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
     user_id = get_jwt_identity()
-    NewAccessToken = create_access_token(identity=user_id)
+    print(user_id, flush=True)
+    NewAccessToken = create_access_token(identity=str(user_id))
 
-    return jsonify(access_token=NewAccessToken), 200
+    response = jsonify({'status': 'Cookie atualizado com sucesso'})
+
+    set_access_cookies(response, NewAccessToken)
+
+    return response, 200
+
+@BP_perfil.route('/api/perfil-changes', methods=["POST"])
+def mudancas():
+    pass
--- app/perfil/services.py
+++ app/perfil/services.py
@@ -1,12 +1,26 @@
 from app.models import user_profile
 from app import db
-from flask_jwt_extended import jwt_required
 from flask import jsonify
+from flask_jwt_extended import jwt_required
 
 def resgatar_dados_usuario(UserId):
     try:
-        user_profile = user_profile.query.filter_by(user_id=UserId).first()
+        user_profiles = user_profile.query.filter_by(user_id=UserId).first()
     except Exception as e:
-        return jsonify({'status': e})
+        return jsonify({'status': "erro ao tentar contatadar o db"}), 500
 
-    return jsonify(user_profile)
+    if not user_profiles:
+        return jsonify({'status': 'usuario não encontrado'})
+
+    user_data = {
+        "nome_completo": getattr(user_profiles, 'nome_completo', None),
+        "primeiro_nome": getattr(user_profiles, 'primeiro_nome', None),
+        "foto_url": getattr(user_profiles, 'foto_url', None),
+        "bio": getattr(user_profiles, 'bio', None),
+        "data_de_nascimento": getattr(user_profiles, 'data_de_nascimento', None),
+        "vendedor": getattr(user_profiles, 'vendedor', None),
+        "nivel": getattr(user_profiles, 'nivel', None),
+        "compras": getattr(user_profiles, 'compras', None),
+    }
+
+    return jsonify(user_data)
